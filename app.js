331d61853bd62b1c9cb049b06dec17a837afd886
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-02-17 19:33:01 +0600

    final push

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-
-
-function calculate() {
-
-
+function validateExpenseInputField() {
+    let flag = false;
     var income = document.getElementById("income").value;
     var food = (document.getElementById("food").value);
     var rent = (document.getElementById("rent").value);
@@ -10,62 +7,91 @@ function calculate() {
     let expense = 0;
     if (income == "" || food == "" || rent == "" || clothes == "") {
         alert("Not accepted");
+        flag = true;
     }
     else if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(clothes)) {
         alert("Not accepted");
+        flag = true;
     }
 
     else if (parseInt(income) < 0 || parseInt(food) < 0 || parseInt(rent) < 0 || parseInt(clothes) < 0) {
         alert("Not accepted");
+        flag = true;
     }
-
-    else {
-        expense = Number(food) + Number(rent) + Number(clothes);
-        console.log(expense);
-
-
-    }
-
-
-
-
-    let balance = Number(income) - expense;
-
-    if (expense < income) {
-        document.getElementById("total-expense").innerHTML = expense;
-        document.getElementById("total-balance").innerHTML = balance;
-
-    } else {
-        alert("Expense more than your income");
-    }
+    return flag;
 
 }
 
-function saveButton() {
+
+function calculate() {
+    var income = document.getElementById("income").value;
+    var food = (document.getElementById("food").value);
+    var rent = (document.getElementById("rent").value);
+    var clothes = (document.getElementById("clothes").value);
+    let expense = 0;
+    if (!validateExpenseInputField()) {
+        // total expense here
+        expense = Number(food) + Number(rent) + Number(clothes);
+        let balance = Number(income) - expense;
+
+        if (expense > income) {
+            alert("Expense more than your income");
+
+        } else {
+            updateExpenseBalance(expense, balance)
+
+        }
+    }
+    else {
+        updateExpenseBalance("", "")
+    }
+}
+
+function updateExpenseBalance(expense, balance) {
+    document.getElementById("total-expense").innerText = expense;
+    document.getElementById("total-balance").innerText = balance;
+}
 
 
+function validateSavingInputField() {
+    let flag = false;
     var saving = document.getElementById("save").value;
     var mainIncome = document.getElementById("income").value;
     var previousBalance = document.getElementById("total-balance").innerText;
     var savingAmount = (saving * mainIncome) / 100;
-
-
     if (saving == "") {
         alert("You must insert between 0 to 100");
+        flag = true;
     }
     else if (isNaN(saving)) {
         alert("you must enter number");
-
+        flag = true;
     }
     else if (parseInt(saving) < 0 || parseInt(saving) > 100) {
         alert("you must insert value between 0 and 100");
+        flag = true;
     }
     else if (savingAmount > previousBalance) {
 
         alert("saving is more than your current balance, save less");
         document.getElementById("total-remaining").innerHTML = "";
+        flag = true;
     }
-    else {
+    return flag;
+}
+
+
+
+// onclick event handler 
+function saveButton() {
+
+    let flag = false;
+    var saving = document.getElementById("save").value;
+    var mainIncome = document.getElementById("income").value;
+    var previousBalance = document.getElementById("total-balance").innerText;
+    var savingAmount = (saving * mainIncome) / 100;
+
+    if (!validateSavingInputField()) {
         document.getElementById("total-saving").innerHTML = savingAmount;
         var lastRemaining = Number(previousBalance) - savingAmount;
         document.getElementById("total-remaining").innerHTML = lastRemaining;
